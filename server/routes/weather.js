b492d836e8c3b07d66df0fa3f5964269980bef5d
sachinyadav878 <sachinyadav2832040@gmail.com>
2025-04-12 16:16:43 +0530

    main

--- server/routes/weather.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getWeather } = require("../controller/weatherController");
-
-router.get("/:city", getWeather);
-
-module.exports = router;
